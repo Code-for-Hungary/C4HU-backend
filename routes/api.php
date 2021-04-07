207d7b3e1c3b67c688e0449332d3214275ec3e48
law909 <balint.lovey@gmail.com>
2021-04-07 02:28:27 +0200

    categories, projects test api endpoints

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Resources\CategoryResource;
+use App\Http\Resources\ProjectResource;
+use App\Models\Category;
+use App\Models\Project;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +20,20 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::get('/categories', function() {
+    return CategoryResource::collection(Category::paginate(3));
+});
+
+Route::get('/categories/{id}', function($id) {
+    return new CategoryResource(Category::findOrFail($id));
+});
+
+Route::get('/projects', function() {
+    return ProjectResource::collection(Project::paginate(3));
+});
+
+Route::get('/projects/{id}', function($id) {
+    return new ProjectResource(Project::findOrFail($id));
 });
